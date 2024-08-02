029c5211946ae5ac1656c30a8d31f3308ab773d3
Advay <advay168@gmail.com>
2024-08-02 16:14:51 +0530

    Make Computer Vision flexible for different window sizes

--- google_selenium_game.py
+++ google_selenium_game.py
@@ -50,6 +50,7 @@ class GoogleSeleniumGame(Game):
 
         self.driver.find_element(By.XPATH, "//*[contains(text(), 'Play')]").click()
         self.canvas = self.driver.find_element(By.TAG_NAME, "canvas")
+        # self.driver.maximize_window()
         time.sleep(0.5)
 
         match self.level:
@@ -124,7 +125,34 @@ class GoogleSeleniumGame(Game):
         self._reload_canvas_dims()
         return np.array(Image.open(io.BytesIO(self.canvas.screenshot_as_png)))
 
+    @staticmethod
+    def parse_rgb_tuple(n: str) -> tuple[int, int, int]:
+        r, g, b = n.split(",")
+        return int(r), int(g), int(b)
+
+    def lookup(self, n: str) -> str | None:
+        threshold = 3
+        r, g, b = self.parse_rgb_tuple(n)
+        h = "headless" if self.headless else "headful"
+        for nn, val in self.lookup_table[h][self.level].items():
+            rr, gg, bb = self.parse_rgb_tuple(nn)
+            if (
+                val != ""
+                and abs(rr - r) <= threshold
+                and abs(gg - g) <= threshold
+                and abs(bb - b) <= threshold
+            ):
+                return val
+
     def _update(self):
+        # Fuzz test different window sizes
+        if True:
+            self.driver.set_window_size(
+                np.random.randint(self.cols * 35, 1500),
+                np.random.randint(self.rows * 35, 900),
+            )
+            time.sleep(1.3)
+
         time.sleep(0.9)
 
         self._move_to(self.initial).perform()
@@ -133,29 +161,29 @@ class GoogleSeleniumGame(Game):
         for x in range(self.cols):
             for y in range(self.rows):
                 n, thumbnail = self._characterise(ss, x, y)
-                if not (n in self.lookup[self.level] and self.lookup[self.level][n] != ""):
+                if self.lookup(n) is None:
                     self._handle_missing(n, thumbnail)
-                self._board[(x, y)] = self.lookup[self.level][n]
+                self._board[(x, y)] = str(self.lookup(n))
 
-    def _handle_missing(self, n, thumbnail):
-        lut = self.lookup[self.level]
+    def _handle_missing(self, n: str, thumbnail: np.ndarray):
         if not self.first_time and not self.settled:
             # Settle board
             self.settled = True
             time.sleep(1)
             a = self._get_board_screenshot()
-            if self._characterise(a, *self.initial)[0] not in lut:
+            if self.lookup(self._characterise(a, *self.initial)[0]) is None:
                 raise GameOver()
 
         # Check if lookup updated in between (occurs with simultaneous processes)
         self._reload_lookup()
-        if n in lut and lut[n] != "":
+        if self.lookup(n):
             return
 
         # Prompt user
         if classification := self._prompt(thumbnail):
             self._reload_lookup()
-            self.lookup[self.level][n] = classification
+            h = "headless" if self.headless else "headful"
+            self.lookup_table[h][self.level][n] = classification
             self._write_lookup()
 
     def _prompt(self, thumbnail):
@@ -207,19 +235,19 @@ class GoogleSeleniumGame(Game):
         root.mainloop()
         return output
 
-    def _characterise(self, img, x, y):
-        pad = 7
+    def _characterise(self, img: np.ndarray, x: int, y: int) -> tuple[str, np.ndarray]:
+        pad = 3
         roi = img[
             y * self.cell_h + pad : (y + 1) * self.cell_h - pad,
             x * self.cell_w + pad : (x + 1) * self.cell_w - pad,
         ]
         r, g, b = roi.reshape(-1, 3).mean(axis=0).astype(int)
-        return str(r * 256 * 256 + g * 256 + b), roi
+        return f"{r},{g},{b}", roi
 
     def _reload_lookup(self):
         with open(self.lookup_file_path) as f:
-            self.lookup = json.load(f)
+            self.lookup_table = json.load(f)
 
     def _write_lookup(self):
         with open(self.lookup_file_path, "w", newline="\n") as f:
-            json.dump(self.lookup, f, indent=2)
+            json.dump(self.lookup_table, f, indent=2)
